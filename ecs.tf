d7535cbc920aada943c6fdb6c9f0d4ab7cb70cc7
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-01-28 00:08:50 -0300

    trying deploy ecs fiap-x

---
+++ ecs.tf
@@ -0,0 +1,41 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "ecs-fiap-x"
+}
+
+resource "aws_ecs_task_definition" "ecs_task" {
+  family                   = "ecs-task"
+  execution_role_arn       = var.labRole
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name        = "ecs-container"
+      image       = "448628038787.dkr.ecr.us-east-1.amazonaws.com/fiapx-api/order"
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+        }
+      ]
+      essential   = true
+    }
+  ])
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = "ecs-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = data.aws_subnets.default.ids
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+}
