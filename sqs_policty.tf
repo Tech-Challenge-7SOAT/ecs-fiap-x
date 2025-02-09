ce7fbab576753baa2aa98235ac51e98d5ae6b113
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-09 18:56:30 -0300

    adding policy to sqs

---
+++ sqs_policty.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_policy" "sqs_policy" {
+  name        = "ecs-sqs-policy"
+  description = "Permissões para ECS acessar o SQS"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "sqs:CreateQueue",
+          "sqs:GetQueueAttributes",
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueUrl"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_sqs_policy" {
+  role       = "LabRole"
+  policy_arn = aws_iam_policy.sqs_policy.arn
+}
